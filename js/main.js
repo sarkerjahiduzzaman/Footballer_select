c3b2476e36d635a182ae81930e2d83230bc2c7f1
sarkerjahiduzzaman <78559065+sarkerjahiduzzaman@users.noreply.github.com>
2022-08-23 12:29:54 +0600

    Condition check in Select function element is more than 5 or not

--- js/main.js
+++ js/main.js
@@ -9,7 +9,25 @@ function selectedPlayerName (selectedBtn , selectedName){
          const element = document.getElementById("list");
          let numb = element.childNodes.length;
         // console.log(numb)
-       
+        //condition checked if orderlist element is less than or equl 5 show the name right side
+        if(numb<=5)
+        {
+            const listParentId = document.getElementById("list");
+        const liElement = document.createElement("li");
+        const textNmae=document.createTextNode(playerNamestring);
+        liElement.appendChild(textNmae);
+        listParentId.appendChild(liElement);
+        // click then disable the buutton
+        const disableBtn = document.getElementById(selectedBtn);
+        disableBtn.disabled = true;
+        disableBtn.style.backgroundColor = "rgb(162, 169, 175)";
+
+        }
+        //else show alert and cannot add element and cannot show the right side
+        else{
+            alert('Boss! You cannot select more than 5 player')
+        }
+
  
         
          
